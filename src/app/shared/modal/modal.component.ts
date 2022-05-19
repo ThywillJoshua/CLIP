7259d7c1d9cd02476260ef9b5b0679eaf1655279
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2022-05-19 18:57:29 +0200

    Complete forms reactive

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -9,9 +9,7 @@ import { ModalService } from 'src/app/services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalID = '';
 
-  constructor(public modal: ModalService, public el: ElementRef) {
-    console.log(el);
-  }
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
